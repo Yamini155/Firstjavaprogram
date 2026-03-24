a0c6fc01ee95c5879a14fdd4a95c541ed5c9ee65
Yamini155 <dyaminiforu@gmail.com>
2026-03-24 08:19:28 +0530

    Class assignment 1

--- src/oops/EmployeeMain.java
+++ src/oops/EmployeeMain.java
@@ -3,8 +3,12 @@ package oops;
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		Employee E1 =new Employee("Yamini",100 ,10000);
-		E1.display();
+		Employee Emp1 =new Employee("Yamini",100 ,10000);
+		Employee Emp2 =new Employee("Priyanka",101 ,20000);
+		Employee Emp3 =new Employee("Sivansh",102 ,30000);
+		Emp1.display();
+		Emp2.display();
+		Emp3.display();
 
 	}
 

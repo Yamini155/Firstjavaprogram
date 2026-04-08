676deddc3c12dedc44f5b367994d12efa6225cea
Yamini155 <dyaminiforu@gmail.com>
2026-04-08 20:23:09 +0530

    Assignment9 modified

--- src/assignment9/Assignment9Class.java
+++ src/assignment9/Assignment9Class.java
@@ -2,6 +2,8 @@ package assignment9;
 
 public class Assignment9Class {
 
+	private static char[] j;
+
 	public static void main(String[] args) {
 		// Question 1
 		
@@ -73,16 +75,200 @@ public class Assignment9Class {
 		
 //		Question 7
 	
-		
-			 
+		int n4=4523;
+		sum=0;
+		while(n4!=0) {
+			int lastDigit=n4%10;
+			sum =sum+lastDigit;
+			n4=n4/10;
+			
 			
 		}
-	
-
-	
+			 
+		System.out.println(sum);
 		
 		
+//	Question 8
+		int n5=234;
+		int product=1;
+		while(n5!=0) {
+			int lastDigit=n5%10;
+			product=product*lastDigit;
+			n5=n5/10;
+			
+		}
+		
+		System.out.println(product);
+		
+		
+		
+//		Question 9
+		int n6 = 153;
+		int count1 = 0;
+		int original = n6;
 
+		while(n6 != 0) {
+		    count1++;
+		    n6 = n6 / 10;
+		}
+
+		
+		n6 = original;
+
+		int sum1 = 0;
+
+		while(n6 != 0) {
+		    int lastDigit = n6 % 10;
+		    int power = 1;
+
+		    for(int i = 1; i <= count1; i++) {
+		        power = power * lastDigit;
+		    }
+
+		    sum1 = sum1 + power;
+		    n6 = n6 / 10;
+		}
+
+		
+		if(sum1 == original) {
+		    System.out.println("Armstrong number");
+		} else {
+		    System.out.println("Not Armstrong number");
+		}
+		
+//	370	
+		
+		int n7 = 370;
+		int count2 = 0;
+		int original1 = n7;
+
+		while(n7 != 0) {
+		    count2++;
+		    n7 = n7 / 10;
+		}
+
+		
+		n7 = original1;
+
+		int sum2 = 0;
+
+		while(n7 != 0) {
+		    int lastDigit = n7 % 10;
+		    int power = 1;
+
+		    for(int i = 1; i <= count2; i++) {
+		        power = power * lastDigit;
+		    }
+
+		    sum2 = sum2 + power;
+		    n7 = n7 / 10;
+		}
+
+		
+		if(sum2 == original1) {
+		    System.out.println("Armstrong number");
+		} else {
+		    System.out.println("Not Armstrong number");
+		}
+		
+//	123	
+		
+		int n8 = 123;
+		int count3 = 0;
+		int original2 = n8;
+
+		while(n8 != 0) {
+		    count3++;
+		    n8 = n8 / 10;
+		}
+
+		
+		n8 = original2;
+
+		int sum3 = 0;
+
+		while(n8 != 0) {
+		    int lastDigit = n8 % 10;
+		    int power = 1;
+
+		    for(int i = 1; i <= count3; i++) {
+		        power = power * lastDigit;
+		    }
+
+		    sum3 = sum3 + power;
+		    n8 = n8 / 10;
+		}
+
+		
+		if(sum3 == original2) {
+		    System.out.println("Armstrong number");
+		} else {
+		    System.out.println("Not Armstrong number");
+		}
+		
+		
+		
+//	Question10
+		
+		int n9=17;
+		boolean isPrime=true;
+		for(int i=2;i<n9;i++) {
+			if(n9%i==0) {
+				isPrime=false;
+			}
+			
+		}
+		
+		if(isPrime) {
+			System.out.println("it is prime number");
+		}else {
+			System.out.println("it is not prime number");
+		}
+		
+		int n11=15;
+		boolean isPrime2=true;
+		for(int i=2;i<n11;i++) {
+			if (n11 % i == 0) {
+		        isPrime2 = false;
+		        break;
+		    }
+			
+		}
+		if(isPrime2) {
+			System.out.println("it is prime number");
+		}else {
+			System.out.println("it is not prime number");
+		}
+		
+		
+		
+//		Question 11
+		int n10=20;
+		
+		for(int i=2;i<=n10;i++) {
+			boolean isPrime1=true;
+			for(int j=2;j<=i-1;j++) {
+				if(i%j==0) {
+					isPrime1=false;
+					break;
+				}
+			}
+			if(isPrime1 ) {
+				System.out.println("prime numbers are"+ " "+ i + "");
+			
+			}
+		
+		
+		}
+		
 	}
+}
+	
+
+	
+		
+		
+
+	
 
 
